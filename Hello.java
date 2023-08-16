7fe474a10b2b70e1126518e52cd6069ee947f9e1
Charis_XO <83030354+charis-byte@users.noreply.github.com>
2023-08-16 10:28:34 +0000

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,9 @@
+/*This is a java program Environment  */
+
+Class HelloWorld{
+  public static void main(String[] args)
+    {
+      System.out.println("Hello Charis On Your Journey to a better life of coding");
+    }
+}
+  
